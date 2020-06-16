2c052a5fde8f61feaa08110b804b5d89693d6d27
Eric Odom <eric@homecareintel.com>
2020-06-16 12:59:23 -0700

    initial commit

---
+++ src/app/main/example/Example.js
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+// import FullCalendar from '@fullcalendar/react'
+// import dayGridPlugin from '@fullcalendar/daygrid'
+
+const useStyles = makeStyles(theme => ({
+	layoutRoot: {}
+}));
+
+function ExamplePage(props) {
+
+	return (
+
+	  <div>
+       {/*<FullCalendar initialView="dayGridMonth" plugins={[ dayGridPlugin ]} />*/}
+    </div>
+
+	);
+}
+
+export default ExamplePage;
